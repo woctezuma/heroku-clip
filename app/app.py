b6b9ccc2aa4758861503676ef1fe2cf3ea20c4f2
Wok <woctezuma@users.noreply.github.com>
2021-01-31 11:35:51 +0100

    Fix bug (TypeError): expected str, not UploadedFile

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from model_utils import compute_probs, predict_class
-from image_utils import load_image
+from image_utils import resize_image
 
 
 def minimal_example(input_image):
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
     if uploaded_file is not None:
-        input_image = load_image(uploaded_file, target_size=[224, 224])
+        input_image = resize_image(uploaded_file, target_size=[224, 224])
         st.image(input_image, caption="Input", use_column_width=True)
 
         imagenet_example(input_image)
