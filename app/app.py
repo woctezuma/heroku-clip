f611a1fd0bcc8a3af2ea82c1d5f772d3083d4a91
Wok <woctezuma@users.noreply.github.com>
2021-01-31 10:03:40 +0100

    Move functions to another file (model_utils.py)

--- app/app.py
+++ app/app.py
@@ -1,35 +1,6 @@
 import streamlit as st
-import torch
-import clip
+from .model_utils import compute_probs
 from .image_utils import load_image
-
-
-def get_device():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    return device
-
-
-def get_model_and_preprocess():
-    model, preprocess = clip.load("ViT-B/32", device=get_device())
-
-    return model, preprocess
-
-
-def compute_probs(input_image, input_text):
-    model, preprocess = get_model_and_preprocess()
-    image = preprocess(input_image).unsqueeze(0).to(get_device())
-    text = clip.tokenize(input_text).to(get_device())
-
-    with torch.no_grad():
-        image_features = model.encode_image(image)
-        text_features = model.encode_text(text)
-
-        logits_per_image, logits_per_text = model(image, text)
-        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-
-    return probs
-
 
 st.title("Deploy OpenAI's CLIP on Heroku.")
 
